c71983b97e16d1fccd378cee85ce7b2dd6b38bed
Jameson Buie <jamesonbuie@gmail.com>
2022-02-15 13:54:53 -0600

    Completed LiterlMathMethodExercise

---
+++ script.js
@@ -0,0 +1,18 @@
+let warmHugs = "Hi. I\'m Olaf and I like warm hugs";
+console.log(warmHugs.toUpperCase());
+console.log(warmHugs.replace("like", "love"));
+warmHugs = "Hi. I\'m Olaf and I love warm hugs"
+
+var beenImpaled = "Oh, look at that. I\'ve been impaled.";
+console.log(beenImpaled.slice(18,37));
+
+var dotDotDot = "...";
+var skullBones = `I don\'t have a skull${dotDotDot}or bones.`;
+console.log(skullBones);
+
+console.log(Math.PI);
+
+let randomNumber = Math.random();
+randomNumber = Math.ceil(randomNumber);
+randomNumber *= 3
+console.log(randomNumber);
